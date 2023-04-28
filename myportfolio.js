f0fcf6fd74a41677715495c7a61acec6ee2b9f28
shiva <shivakrishnabandari59@gmail.com>
2023-04-28 22:43:58 +0530

    commit

---
+++ myportfolio.js
@@ -0,0 +1,11 @@
+/*let profileview = document.getElementById();
+let resume = document.getElementById("getresume");
+
+let count = localStorage.getItem('count') || 0;
+profileview.textContent = count;
+
+resume.addEventListener('click', () => {
+    count++;
+    localStorage.setItem('count', count);
+    profileview.textContent = count;
+});*/
